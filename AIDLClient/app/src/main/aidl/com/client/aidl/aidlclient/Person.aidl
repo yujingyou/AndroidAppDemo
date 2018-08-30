2c2f9890754f19ed7cbd1ad0f0a2b6b8be8cebd5
yujingyou <yujingyou157@163.com>
2018-08-30 14:29:15 +0800

    调试至Server和client可通讯

--- AIDLClient/app/src/main/aidl/com/client/aidl/aidlclient/Person.aidl
+++
@@ -1,6 +0,0 @@
-// Person.aidl
-package com.client.aidl.aidlclient;
-
-// Declare any non-default types here with import statements
-parcelable Person;
-
--- AIDLClient/app/src/main/java/com/client/aidl/aidlclient/MainActivity.java
+++ AIDLClient/app/src/main/java/com/client/aidl/aidlclient/MainActivity.java
@@ -1,20 +1,36 @@
 package com.client.aidl.aidlclient;
 
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.IBinder;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.service.aidl.aidlservice.MyAidlService;
+import com.service.aidl.aidlservice.IMyAidlInterface;
+import com.service.aidl.aidlservice.Person;
+
+import java.text.BreakIterator;
+import java.util.List;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     private IMyAidlInterface mAidl;
-
+    TextView mHellow;
     private ServiceConnection mConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
             //连接后拿到 Binder，转换成 AIDL，在不同进程会返回个代理
+
             mAidl = IMyAidlInterface.Stub.asInterface(service);
+            System.out.println("onServiceConnected"+mAidl);
         }
 
         @Override
@@ -26,7 +42,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent1 = new Intent(getApplicationContext(), MyAidlService.class);
-        bindService(intent1, mConnection, BIND_AUTO_CREATE);
+
+        System.out.println("onCreate"+mAidl);
+
+        Intent intent = new Intent("com.service.aidl.aidlservice.IMyAidlInterface");
+
+        //Android5.0后无法只通过隐式Intent绑定远程Service
+        //需要通过setPackage()方法指定包名
+        intent.setPackage("com.service.aidl.aidlservice");
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+        Button button2 = (Button) findViewById(R.id.button);
+        mHellow = (TextView)findViewById(R.id.txt1);
+        System.out.println("onCreate"+mAidl);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addPerson();
+
+            }
+        });
+        }
+
+    public void addPerson() {
+        Random random = new Random();
+        Person person = new Person("shixin" + random.nextInt(10));
+
+        try {
+            mAidl.addPerson(person);
+            List<Person> personList = mAidl.getPersonList();
+            mHellow.setText(personList.toString());
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 }
